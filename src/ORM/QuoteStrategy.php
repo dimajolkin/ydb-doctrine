ac20f76f2eba07041e9928a05b7f834e6d3f90d2
dimajolkin <dimajolkin@rambler.ru>
2022-09-19 16:56:28 +0300

    added QuoteStrategy

---
+++ src/ORM/QuoteStrategy.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Dimajolkin\YdbDoctrine\ORM;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\ORM\Mapping\ClassMetadata;
+use Doctrine\ORM\Mapping\DefaultQuoteStrategy;
+
+class QuoteStrategy extends DefaultQuoteStrategy
+{
+    public function getColumnAlias($columnName, $counter, AbstractPlatform $platform, ?ClassMetadata $class = null): string
+    {
+        return $columnName;
+    }
+}
